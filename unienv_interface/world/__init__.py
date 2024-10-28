15af5e3e89ca5f22c60f5e8b59d1a6b58a2f0ef3
Yunhao Cao <realquantumcookie@gmail.com>
2024-10-27 22:31:21 -0400

    Add TaskWrapper

--- unienv_interface/world/__init__.py
+++ unienv_interface/world/__init__.py
@@ -1,5 +1,5 @@
 from .actor import Actor, ActorWrapper, FuncActor, FuncActorWrapper
 from .sensor import Sensor, FuncSensor
-from .task import Task, FuncTask, FuncTaskWrapper
+from .task import Task, TaskWrapper, FuncTask, FuncTaskWrapper
 from .world import World, FuncWorld
 from .env_utils import WorldBasedFuncEnv
--- unienv_interface/world/task.py
+++ unienv_interface/world/task.py
@@ -45,6 +45,30 @@ class Task(ABC, Generic[RewardType, TerminationType]):
         """Close the task."""
         pass
 
+    def __del__(self):
+        self.close()
+
+    # ========== Wrapper methods ==========
+    @property
+    def unwrapped(self) -> "Task":
+        return self
+    
+    @property
+    def prev_wrapper_layer(self) -> Optional["Task"]:
+        return None
+    
+    def has_wrapper_attr(self, name: str) -> bool:
+        """Checks if the attribute `name` exists in the environment."""
+        return hasattr(self, name)
+    
+    def get_wrapper_attr(self, name: str) -> Any:
+        """Gets the attribute `name` from the environment."""
+        return getattr(self, name)
+    
+    def set_wrapper_attr(self, name: str, value: Any):
+        """Sets the attribute `name` on the environment with `value`."""
+        setattr(self, name, value)
+
 TaskStateT = TypeVar("TaskStateT")
 class FuncTask(
     ABC,
@@ -156,15 +180,102 @@ class FuncTask(
         """Sets the attribute `name` on the environment with `value`."""
         setattr(self, name, value)
 
-TaskWrapperStateT = TypeVar("TaskWrapperStateT")
+
 TaskWrapperRewardType = TypeVar("TaskWrapperRewardType")
 TaskWrapperTerminationType = TypeVar("TaskWrapperTerminationType")
+
+class TaskWrapper(
+    Generic[
+        TaskWrapperRewardType, TaskWrapperTerminationType,
+        RewardType, TerminationType
+    ],
+    Task[TaskWrapperRewardType, TaskWrapperTerminationType]
+):
+    def __init__(
+        self,
+        task : Task[RewardType, TerminationType]
+    ):
+        self.task = task
+        self._observation_space : Optional[DictSpace[BDeviceType, BDtypeType, BRNGType]] = None
+    
+    @property
+    def observation_space(self) -> Optional[DictSpace[BDeviceType, BDtypeType, BRNGType]]:
+        return self._observation_space or self.task.observation_space
+    
+    @observation_space.setter
+    def observation_space(self, value : Optional[DictSpace[BDeviceType, BDtypeType, BRNGType]]) -> None:
+        self._observation_space = value
+
+    def update(self, last_step_elapsed : float) -> None:
+        self.task.update(last_step_elapsed)
+    
+    def control_update(self, last_control_step_elapsed : float) -> None:
+        self.task.control_update(last_control_step_elapsed)
+    
+    def get_data(self) -> Optional[Dict[str, Any]]:
+        return self.task.get_data()
+    
+    def get_reward(self) -> TaskWrapperRewardType:
+        return self.task.get_reward()
+    
+    def get_termination(self) -> TaskWrapperTerminationType:
+        return self.task.get_termination()
+    
+    def get_truncation(self) -> TaskWrapperTerminationType:
+        return self.task.get_truncation()
+    
+    def reset(self) -> None:
+        self.task.reset()
+    
+    def close(self) -> None:
+        self.task.close()
+
+    # ========== Wrapper methods ==========
+    @property
+    def unwrapped(self) -> "Task":
+        return self.task.unwrapped
+    
+    @property
+    def prev_wrapper_layer(self) -> "Task[RewardType, TerminationType]":
+        return self.task
+    
+    def has_wrapper_attr(self, name: str) -> bool:
+        return hasattr(self, name) or self.task.has_wrapper_attr(name)
+    
+    def get_wrapper_attr(self, name: str) -> Any:
+        if hasattr(self, name):
+            return getattr(self, name)
+        else:
+            try:
+                return self.task.get_wrapper_attr(name)
+            except AttributeError as e:
+                raise AttributeError(
+                    f"wrapper {type(self).__name__} has no attribute {name!r}"
+                ) from e
+
+    def set_wrapper_attr(self, name: str, value: Any):
+        sub_task = self
+        attr_set = False
+
+        while attr_set is False and sub_task is not None:
+            if hasattr(sub_task, name):
+                setattr(sub_task, name, value)
+                attr_set = True
+            else:
+                sub_task = sub_task.prev_wrapper_layer
+
+        if attr_set is False and sub_task is None:
+            raise AttributeError(
+                f"wrapper {type(self).__name__} has no attribute {name!r}"
+            )
+
+TaskWrapperStateT = TypeVar("TaskWrapperStateT")
 class FuncTaskWrapper(
-    ABC,
     Generic[
         TaskWrapperStateT, TaskWrapperRewardType, TaskWrapperTerminationType,
         StateType, TaskStateT, RewardType, TerminationType
-    ]
+    ],
+    FuncTask[StateType, TaskWrapperStateT, TaskWrapperRewardType, TaskWrapperTerminationType]
 ):
     def __init__(
         self,
