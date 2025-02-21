2ff82c1a09d5b3090a9b420a342bd2de9a02cb74
Yunhao Cao <realquantumcookie@gmail.com>
2025-02-21 17:40:46 -0500

    Change testing params

--- tests_mjxplayground/test_dmsuite.py
+++ tests_mjxplayground/test_dmsuite.py
@@ -2,9 +2,9 @@ from utils import construct_env_from_name, perform_env_test
 import pytest
 
 @pytest.mark.parametrize("env_name", ["CartpoleBalance"])
-@pytest.mark.parametrize("n_envs", [8, 16])
+@pytest.mark.parametrize("n_envs", [16, 32])
 @pytest.mark.parametrize("seed", [0, 1024])
-@pytest.mark.parametrize("jit", [True, False])
+@pytest.mark.parametrize("jit", [True]) # If not traced it will be tooooooo slow
 def test_dmsuite(
     env_name : str,
     n_envs : int,
