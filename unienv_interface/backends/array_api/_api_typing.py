6a23ad772ca816aa66f766fc588c94c77057a0c1
Yunhao Cao <realquantumcookie@gmail.com>
2025-05-21 16:37:12 -0400

    Add manipulation functions

--- unienv_interface/backends/array_api/_api_typing.py
+++ unienv_interface/backends/array_api/_api_typing.py
@@ -4378,3 +4378,391 @@ class ArrayAPINamespace(Protocol[_NAMESPACE_ARRAY, _NAMESPACE_DEVICE, _NAMESPACE
         .. versionchanged:: 2023.12
         Restricted ``axis`` to only negative integers.
         """
+    
+    """
+    Manipulation Functions
+    https://github.com/data-apis/array-api/blob/main/src/array_api_stubs/_2024_12/manipulation_functions.py
+    """
+    @abstractmethod
+    def broadcast_arrays(
+        self: _NAMESPACE_C,
+        *arrays: _NAMESPACE_ARRAY
+    ) -> List[_NAMESPACE_ARRAY]:
+        """
+        Broadcasts one or more arrays against one another.
+
+        Parameters
+        ----------
+        arrays: _NAMESPACE_ARRAY
+            an arbitrary number of to-be broadcasted arrays.
+
+        Returns
+        -------
+        out: List[array]
+            a list of broadcasted arrays. Each array must have the same shape. Each array must have the same dtype as its corresponding input array.
+        """
+
+    @abstractmethod
+    def broadcast_to(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY, /, shape: Tuple[int, ...]
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Broadcasts an array to a specified shape.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            array to broadcast. Must be capable of being broadcast to the specified ``shape`` (see :ref:`broadcasting`). If the array is incompatible with the specified shape, the function must raise an exception.
+        shape: Tuple[int, ...]
+            array shape.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an array having the specified shape. Must have the same data type as ``x``.
+
+        .. versionchanged:: 2024.12
+        Clarified broadcast behavior.
+        """
+
+    @abstractmethod
+    def concat(
+        self: _NAMESPACE_C,
+        arrays: Union[Tuple[_NAMESPACE_ARRAY, ...], List[_NAMESPACE_ARRAY]], /, *, axis: Optional[int] = 0
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Joins a sequence of arrays along an existing axis.
+
+        Parameters
+        ----------
+        arrays: Union[Tuple[array, ...], List[array]]
+            input arrays to join. The arrays must have the same shape, except in the dimension specified by ``axis``.
+        axis: Optional[int]
+            axis along which the arrays will be joined. If ``axis`` is ``None``, arrays must be flattened before concatenation. If ``axis`` is negative, the function must determine the axis along which to join by counting from the last dimension. Default: ``0``.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an output array containing the concatenated values. If the input arrays have different data types, normal :ref:`type-promotion` must apply. If the input arrays have the same data type, the output array must have the same data type as the input arrays.
+
+            .. note::
+            This specification leaves type promotion between data type families (i.e., ``intxx`` and ``floatxx``) unspecified.
+        """
+
+    @abstractmethod
+    def expand_dims(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY, /, *, axis: int = 0
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Expands the shape of an array by inserting a new axis (dimension) of size one at the position specified by ``axis``.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array.
+        axis: int
+            axis position (zero-based). If ``x`` has rank (i.e, number of dimensions) ``N``, a valid ``axis`` must reside on the closed-interval ``[-N-1, N]``. If provided a negative ``axis``, the axis position at which to insert a singleton dimension must be computed as ``N + axis + 1``. Hence, if provided ``-1``, the resolved axis position must be ``N`` (i.e., a singleton dimension must be appended to the input array ``x``). If provided ``-N-1``, the resolved axis position must be ``0`` (i.e., a singleton dimension must be prepended to the input array ``x``).
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an expanded output array having the same data type as ``x``.
+
+        Raises
+        ------
+        IndexError
+            If provided an invalid ``axis`` position, an ``IndexError`` should be raised.
+        """
+
+    @abstractmethod
+    def flip(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY, /, *, axis: Optional[Union[int, Tuple[int, ...]]] = None
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Reverses the order of elements in an array along the given axis. The shape of the array must be preserved.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array.
+        axis: Optional[Union[int, Tuple[int, ...]]]
+            axis (or axes) along which to flip. If ``axis`` is ``None``, the function must flip all input array axes. If ``axis`` is negative, the function must count from the last dimension. If provided more than one axis, the function must flip only the specified axes. Default: ``None``.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an output array having the same data type and shape as ``x`` and whose elements, relative to ``x``, are reordered.
+        """
+
+    @abstractmethod
+    def moveaxis(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY,
+        source: Union[int, Tuple[int, ...]],
+        destination: Union[int, Tuple[int, ...]],
+        /,
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Moves array axes (dimensions) to new positions, while leaving other axes in their original positions.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array.
+        source: Union[int, Tuple[int, ...]]
+            Axes to move. Provided axes must be unique. If ``x`` has rank (i.e, number of dimensions) ``N``, a valid axis must reside on the half-open interval ``[-N, N)``.
+        destination: Union[int, Tuple[int, ...]]
+            indices defining the desired positions for each respective ``source`` axis index. Provided indices must be unique. If ``x`` has rank (i.e, number of dimensions) ``N``, a valid axis must reside on the half-open interval ``[-N, N)``.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an array containing reordered axes. The returned array must have the same data type as ``x``.
+
+        Notes
+        -----
+
+        .. versionadded:: 2023.12
+        """
+
+    @abstractmethod
+    def permute_dims(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY, /, axes: Tuple[int, ...]
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Permutes the axes (dimensions) of an array ``x``.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array.
+        axes: Tuple[int, ...]
+            tuple containing a permutation of ``(0, 1, ..., N-1)`` where ``N`` is the number of axes (dimensions) of ``x``.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an array containing the axes permutation. The returned array must have the same data type as ``x``.
+        """
+
+    @abstractmethod
+    def repeat(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY,
+        repeats: Union[int, _NAMESPACE_ARRAY],
+        /,
+        *,
+        axis: Optional[int] = None,
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Repeats each element of an array a specified number of times on a per-element basis.
+
+        .. admonition:: Data-dependent output shape
+            :class: important
+
+            When ``repeats`` is an array, the shape of the output array for this function depends on the data values in the ``repeats`` array; hence, array libraries which build computation graphs (e.g., JAX, Dask, etc.) may find this function difficult to implement without knowing the values in ``repeats``. Accordingly, such libraries may choose to omit support for ``repeats`` arrays; however, conforming implementations must support providing a literal ``int``. See :ref:`data-dependent-output-shapes` section for more details.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array containing elements to repeat.
+        repeats: Union[int, array]
+            the number of repetitions for each element.
+
+            If ``axis`` is ``None``, let ``N = prod(x.shape)`` and
+
+            -   if ``repeats`` is an array, ``repeats`` must be broadcast compatible with the shape ``(N,)`` (i.e., be a one-dimensional array having shape ``(1,)`` or ``(N,)``).
+            -   if ``repeats`` is an integer, ``repeats`` must be broadcasted to the shape `(N,)`.
+
+            If ``axis`` is not ``None``, let ``M = x.shape[axis]`` and
+
+            -   if ``repeats`` is an array, ``repeats`` must be broadcast compatible with the shape ``(M,)`` (i.e., be a one-dimensional array having shape ``(1,)`` or ``(M,)``).
+            -   if ``repeats`` is an integer, ``repeats`` must be broadcasted to the shape ``(M,)``.
+
+            If ``repeats`` is an array, the array must have an integer data type.
+
+            .. note::
+            For specification-conforming array libraries supporting hardware acceleration, providing an array for ``repeats`` may cause device synchronization due to an unknown output shape. For those array libraries where synchronization concerns are applicable, conforming array libraries are advised to include a warning in their documentation regarding potential performance degradation when ``repeats`` is an array.
+
+        axis: Optional[int]
+            the axis (dimension) along which to repeat elements. If ``axis`` is `None`, the function must flatten the input array ``x`` and then repeat elements of the flattened input array and return the result as a one-dimensional output array. A flattened input array must be flattened in row-major, C-style order. Default: ``None``.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an output array containing repeated elements. The returned array must have the same data type as ``x``. If ``axis`` is ``None``, the returned array must be a one-dimensional array; otherwise, the returned array must have the same shape as ``x``, except for the axis (dimension) along which elements were repeated.
+
+        Notes
+        -----
+
+        .. versionadded:: 2023.12
+        """
+
+    @abstractmethod
+    def reshape(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY, /, shape: Tuple[int, ...], *, copy: Optional[bool] = None
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Reshapes an array without changing its data.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array to reshape.
+        shape: Tuple[int, ...]
+            a new shape compatible with the original shape. One shape dimension is allowed to be ``-1``. When a shape dimension is ``-1``, the corresponding output array shape dimension must be inferred from the length of the array and the remaining dimensions.
+        copy: Optional[bool]
+            whether or not to copy the input array. If ``True``, the function must always copy (see :ref:`copy-keyword-argument`). If ``False``, the function must never copy. If ``None``, the function must avoid copying, if possible, and may copy otherwise. Default: ``None``.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an output array having the same data type and elements as ``x``.
+
+        Raises
+        ------
+        ValueError
+            If ``copy=False`` and a copy would be necessary, a ``ValueError``
+            should be raised.
+        """
+
+    @abstractmethod
+    def roll(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY,
+        /,
+        shift: Union[int, Tuple[int, ...]],
+        *,
+        axis: Optional[Union[int, Tuple[int, ...]]] = None,
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Rolls array elements along a specified axis. Array elements that roll beyond the last position are re-introduced at the first position. Array elements that roll beyond the first position are re-introduced at the last position.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array.
+        shift: Union[int, Tuple[int, ...]]
+            number of places by which the elements are shifted. If ``shift`` is a tuple, then ``axis`` must be a tuple of the same size, and each of the given axes must be shifted by the corresponding element in ``shift``. If ``shift`` is an ``int`` and ``axis`` a tuple, then the same ``shift`` must be used for all specified axes. If a shift is positive, then array elements must be shifted positively (toward larger indices) along the dimension of ``axis``. If a shift is negative, then array elements must be shifted negatively (toward smaller indices) along the dimension of ``axis``.
+        axis: Optional[Union[int, Tuple[int, ...]]]
+            axis (or axes) along which elements to shift. If ``axis`` is ``None``, the array must be flattened, shifted, and then restored to its original shape. Default: ``None``.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an output array having the same data type as ``x`` and whose elements, relative to ``x``, are shifted.
+        """
+
+    @abstractmethod
+    def squeeze(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY, /, axis: Union[int, Tuple[int, ...]]
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Removes singleton dimensions (axes) from ``x``.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array.
+        axis: Union[int, Tuple[int, ...]]
+            axis (or axes) to squeeze.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an output array having the same data type and elements as ``x``.
+
+        Raises
+        ------
+        ValueError
+            If a specified axis has a size greater than one (i.e., it is not a
+            singleton dimension), a ``ValueError`` should be raised.
+        """
+
+    @abstractmethod
+    def stack(
+        self: _NAMESPACE_C,
+        arrays: Union[Tuple[_NAMESPACE_ARRAY, ...], List[_NAMESPACE_ARRAY]], /, *, axis: int = 0
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Joins a sequence of arrays along a new axis.
+
+        Parameters
+        ----------
+        arrays: Union[Tuple[array, ...], List[array]]
+            input arrays to join. Each array must have the same shape.
+        axis: int
+            axis along which the arrays will be joined. Providing an ``axis`` specifies the index of the new axis in the dimensions of the result. For example, if ``axis`` is ``0``, the new axis will be the first dimension and the output array will have shape ``(N, A, B, C)``; if ``axis`` is ``1``, the new axis will be the second dimension and the output array will have shape ``(A, N, B, C)``; and, if ``axis`` is ``-1``, the new axis will be the last dimension and the output array will have shape ``(A, B, C, N)``. A valid ``axis`` must be on the interval ``[-N, N)``, where ``N`` is the rank (number of dimensions) of ``x``. If provided an ``axis`` outside of the required interval, the function must raise an exception. Default: ``0``.
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            an output array having rank ``N+1``, where ``N`` is the rank (number of dimensions) of ``x``. If the input arrays have different data types, normal :ref:`type-promotion` must apply. If the input arrays have the same data type, the output array must have the same data type as the input arrays.
+
+            .. note::
+            This specification leaves type promotion between data type families (i.e., ``intxx`` and ``floatxx``) unspecified.
+        """
+
+    @abstractmethod
+    def tile(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY, repetitions: Tuple[int, ...], /
+    ) -> _NAMESPACE_ARRAY:
+        """
+        Constructs an array by tiling an input array.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array.
+        repetitions: Tuple[int, ...]
+            number of repetitions along each axis (dimension).
+
+            Let ``N = len(x.shape)`` and ``M = len(repetitions)``.
+
+            If ``N > M``, the function must prepend ones until all axes (dimensions) are specified (e.g., if ``x`` has shape ``(8,6,4,2)`` and ``repetitions`` is the tuple ``(3,3)``, then ``repetitions`` must be treated as ``(1,1,3,3)``).
+
+            If ``N < M``, the function must prepend singleton axes (dimensions) to ``x`` until ``x`` has as many axes (dimensions) as ``repetitions`` specifies (e.g., if ``x`` has shape ``(4,2)`` and ``repetitions`` is the tuple ``(3,3,3,3)``, then ``x`` must be treated as if it has shape ``(1,1,4,2)``).
+
+        Returns
+        -------
+        out: _NAMESPACE_ARRAY
+            a tiled output array. The returned array must have the same data type as ``x`` and must have a rank (i.e., number of dimensions) equal to ``max(N, M)``. If ``S`` is the shape of the tiled array after prepending singleton dimensions (if necessary) and ``r`` is the tuple of repetitions after prepending ones (if necessary), then the number of elements along each axis (dimension) must satisfy ``S[i]*r[i]``, where ``i`` refers to the ``i`` th axis (dimension).
+
+        Notes
+        -----
+
+        .. versionadded:: 2023.12
+        """
+
+    @abstractmethod
+    def unstack(
+        self: _NAMESPACE_C,
+        x: _NAMESPACE_ARRAY, /, *, axis: int = 0
+    ) -> Tuple[_NAMESPACE_ARRAY, ...]:
+        """
+        Splits an array into a sequence of arrays along the given axis.
+
+        Parameters
+        ----------
+        x: _NAMESPACE_ARRAY
+            input array.
+        axis: int
+            axis along which the array will be split. A valid ``axis`` must be on the interval ``[-N, N)``, where ``N`` is the rank (number of dimensions) of ``x``. If provided an ``axis`` outside of the required interval, the function must raise an exception. Default: ``0``.
+
+        Returns
+        -------
+        out: Tuple[array, ...]
+            tuple of slices along the given dimension. All the arrays have the same shape.
+
+        Notes
+        -----
+
+        .. versionadded:: 2023.12
+        """
