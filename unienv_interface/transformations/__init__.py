f925c4e8b95a1affbdd5afefe3f18519d0aff715
Yunhao Cao <realquantumcookie@gmail.com>
2025-07-02 14:55:07 -0400

    Add `DictTransformation`

--- unienv_interface/transformations/__init__.py
+++ unienv_interface/transformations/__init__.py
@@ -2,3 +2,4 @@ from .transformation import DataTransformation
 from .rescale import RescaleTransformation
 from .filter_dict import DictIncludeKeyTransformation, DictExcludeKeyTransformation
 from .batch_and_unbatch import BatchifyTransformation, UnBatchifyTransformation
+from .dict_transform import DictTransformation
