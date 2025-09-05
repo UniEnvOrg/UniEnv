a5bc88bf098767b99d331f26958022ef38c20eb4
Yunhao Cao <realquantumcookie@gmail.com>
2025-09-05 15:56:25 -0400

    Add `CombinedFuncNode`

--- unienv_interface/world/__init__.py
+++ unienv_interface/world/__init__.py
@@ -3,3 +3,4 @@ from .node import WorldNode
 from .combined_node import CombinedWorldNode
 from .funcworld import FuncWorld
 from .funcnode import FuncWorldNode
+from .combined_funcnode import CombinedFuncWorldNode
