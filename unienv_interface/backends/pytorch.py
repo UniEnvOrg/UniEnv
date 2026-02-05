a0b2918a877b9d68630b4a4457c04ba7aff4b352
Yunhao Cao <realquantumcookie@gmail.com>
2026-02-05 10:39:51 -0500

    Update XBArray PyTorch backend import

--- unienv_interface/backends/pytorch.py
+++ unienv_interface/backends/pytorch.py
@@ -1,7 +1,7 @@
 try:
-    from xbarray.backends.pytorch import PytorchComputeBackend as XBPytorchBackend
+    from xbarray.backends.pytorch import PyTorchComputeBackend as XBPytorchBackend
 except ImportError:
-    from xbarray.pytorch import PytorchComputeBackend as XBPytorchBackend
+    from xbarray.pytorch import PyTorchComputeBackend as XBPytorchBackend
 from xbarray import ComputeBackend
 
 from typing import Union
