87eb679f863191bd6869471d8bcceda09ce1a284
Yunhao Cao <realquantumcookie@gmail.com>
2026-02-06 17:51:21 -0500

    Better HF support

--- unienv_data/integrations/huggingface.py
+++ unienv_data/integrations/huggingface.py
@@ -1,7 +1,9 @@
-from typing import Optional
+from typing import Optional, Any
 from datasets import Dataset as HFDataset
 from unienv_interface.backends import ComputeBackend, BArrayType, BDeviceType, BDtypeType, BRNGType
+from unienv_interface.space import Space
 from unienv_interface.space.space_utils import construct_utils as scu, batch_utils as sbu
+from unienv_interface.utils.dict_util import nested_get
 from unienv_data.base import BatchBase, BatchT
 
 __all__ = [
@@ -17,22 +19,34 @@ class HFAsUniEnvDataset(BatchBase[BatchT, BArrayType, BDeviceType, BDtypeType, B
 
     is_mutable = False
 
-    def __init__(
-        self,
+    @staticmethod
+    def create(
         hf_dataset: HFDataset,
         backend: ComputeBackend[BArrayType, BDeviceType, BDtypeType, BRNGType],
         device : Optional[BDeviceType] = None,
-    ) -> None:
+    ) -> "HFAsUniEnvDataset[BArrayType, BDeviceType, BDtypeType, BRNGType]":
         kwargs = {}
-        if backend.simplified_name != 'numpy':
+        if backend.simplified_name != 'numpy' and device is not None:
             kwargs['device'] = device
-        self.hf_dataset = hf_dataset.with_format(
-            self.BACKEND_TO_FORMAT_MAP[backend.simplified_name],
+        
+        hf_dataset = hf_dataset.with_format(
+            __class__.BACKEND_TO_FORMAT_MAP[backend.simplified_name],
             **kwargs
         )
-        first_data = self.hf_dataset[0]
-        super().__init__(
+        first_data = hf_dataset[0]
+        return __class__(
+            hf_dataset,
             scu.construct_space_from_data(first_data, backend)
+        )
+
+    def __init__(
+        self,
+        hf_dataset: HFDataset,
+        space : Space[Any, BDeviceType, BDtypeType, BRNGType],
+    ) -> None:
+        self.hf_dataset = hf_dataset
+        super().__init__(
+            space
         )
     
     def __len__(self) -> int:
@@ -45,3 +59,10 @@ class HFAsUniEnvDataset(BatchBase[BatchT, BArrayType, BDeviceType, BDtypeType, B
     
     def get_at(self, idx):
         return self.get_at_with_metadata(idx)[0]
+    
+    def get_column(self, nested_keys):
+        single_key = ".".join(nested_keys)
+        return __class__(
+            self.hf_dataset.select_columns(single_key),
+            self.single_space[single_key]
+        )
